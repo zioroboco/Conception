6a1123376b9388598741165ef96d23116f2fc547
Louis <zioroboco@gmail.com>
2016-09-21 14:24:57 +1000

    First look at concepts

---
+++ Assets/Scripts/Concept.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Concept : MonoBehaviour
+{
+	[SerializeField] GameObject Label;
+
+	Concept parentConcept;
+	
+	bool rooted = false;
+
+	void Awake ()
+	{
+	}
+
+	void Root ()
+	{
+		if (rooted) return;
+	}
+}
