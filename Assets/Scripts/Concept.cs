293ddf6749174ecc0b9a9272941846a9c2328a92
Louis <zioroboco@gmail.com>
2016-09-26 08:34:50 +1000

    Map construction from JSON

--- Assets/Scripts/Concept.cs
+++ Assets/Scripts/Concept.cs
@@ -1,20 +1,61 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class Concept : MonoBehaviour
 {
 	[SerializeField] GameObject Label;
 
-	Concept parentConcept;
-	
+	Dictionary<string,string> feedback;
+
 	bool rooted = false;
+	TextMesh label;
 
 	void Awake ()
 	{
+		label = GetComponentInChildren<TextMesh>();
 	}
 
 	void Root ()
 	{
 		if (rooted) return;
+		// todo: root me
+		if (!this.IsMapRoot())
+			this.GetComponent<Rigidbody2D>().isKinematic = false;
+	}
+
+	public GameObject ParentGameObject () {
+		return this.transform.parent.gameObject;
+	}
+
+	public Concept ParentConcept () {
+		return this.ParentGameObject().GetComponent<Concept>();
+	}
+
+	public bool IsMapRoot ()
+	{
+		return !ParentConcept();
+	}
+
+	public void SetFeedback(Dictionary<string, string> feedback)
+	{
+		this.feedback = feedback;
+	}
+
+	public string GetFeedback(string parent)
+	{
+		string message;
+		feedback.TryGetValue(parent, out message);
+		return message;
+	}
+
+    public void SetName(string name)
+    {
+		label.text = name;
+    }
+
+	public void SetParent(Concept parentConcept)
+	{
+		this.transform.parent = parentConcept.transform;
 	}
 }
--- Assets/Scripts/ConceptMap.cs
+++ Assets/Scripts/ConceptMap.cs
@@ -1,14 +1,113 @@
 ﻿using System;
 using UnityEngine;
 using LitJson;
+using System.Collections.Generic;
 
 public class ConceptMap : MonoBehaviour
 {
-	[SerializeField] TextAsset SourceFile;
-	[SerializeField] GameObject ConceptPrefab;
+	[SerializeField]
+	TextAsset SourceFile;
+	[SerializeField]
+	GameObject ConceptPrefab;
+
+	JsonData feedback;
+	Concept root;
 
 	void Awake ()
 	{
-		JsonData data = JsonMapper.ToObject(SourceFile.text);
+		Load(SourceFile);
+	}
+
+	/// Build a map from the given source file.
+	void Load (TextAsset source)
+	{
+		JsonData json = JsonMapper.ToObject(source.text);
+
+		this.feedback = json["feedback"];
+
+		this.root = Populate(null, json["map"][0]);
+		this.root.transform.parent = this.transform;
+	}
+
+	/// Build a concept map from the given partial tree.
+	Concept Populate (Concept parent, JsonData treeNode)
+	{
+		// Create the topmost new concept.
+		string name = (string) treeNode["name"];
+		Concept concept = CreateConcept(name);
+
+		// Parent the new concept.
+		if (parent)
+		{
+			concept.SetParent(parent);
+		}
+
+		// Recursively build the subtrees in "children", with this new concept as parent.
+		if (!IsLeaf(treeNode))
+		{
+			JsonData subtrees = treeNode["children"];
+			for (int i = 0; i < subtrees.Count; i++)
+			{
+				Populate(concept, subtrees[i]);
+			}
+		}
+
+		return concept;
+	}
+
+	/// Returns true if this node has no array of children.
+	bool IsLeaf (JsonData node)
+	{
+		return node.Count == 1;
+	}
+
+	/// Instantiate and initialise a new Concept prefab.
+	Concept CreateConcept (string name)
+	{
+		// Instantiate and name the gameobject.
+		GameObject conceptGameObject = Instantiate(ConceptPrefab);
+		conceptGameObject.name = ("Concept (" + name + ")");
+
+		// Set the name on the label.
+		Concept concept = conceptGameObject.GetComponent<Concept>();
+		concept.SetName(name);
+
+		// Create a feedback dictionary from this concept's "categories" array.
+		JsonData categoriesArray = CategoriesArrayForChild(name);
+		concept.SetFeedback(BuildFeedbackDictionary(categoriesArray));
+
+		return concept;
+	}
+
+	/// Searches the "feedback" list for an entry with the given child, and returns its "categories" array.
+	JsonData CategoriesArrayForChild(string child)
+	{	
+		for (int i = 0; i < feedback.Count; i++)
+		{
+			JsonData entry = feedback[i];
+			if ((string) entry["child"] == child)
+			{
+				return entry["categories"];
+			}
+		}
+
+		return null;
+	}
+
+	/// Returns a dictionary of parents to feedback messages from the given "categories" array.
+	Dictionary<string,string> BuildFeedbackDictionary (JsonData feedbackArray)
+	{
+		Dictionary<string,string> feedbackDictionary = new Dictionary<string,string>();
+
+		if (feedbackArray != null)
+		{
+			for (int i = 0; i < feedbackArray.Count; i++)
+			{
+				JsonData entry = feedbackArray[i];
+				feedbackDictionary.Add((string) entry[0], (string) entry[1]);
+			}
+		}
+
+		return feedbackDictionary;
 	}
 }
