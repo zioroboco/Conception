c828f4d1a95cf87647198ca6f5b8c52a8c739aa2
Louis <louis@Galileo.local>
2016-10-15 04:31:54 +1100

    Static class to keep lists of scores

---
+++ Assets/Scripts/Scorekeeper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Scorekeeper : MonoBehaviour {
+	
+	public static List<int> correct;
+	public static List<int> attempts;
+	
+	public void AddScore(int numberCorrect, int numberOfAttempts)
+	{
+		correct.Add(numberCorrect);
+		attempts.Add(numberOfAttempts);
+	}
+}
