de5f8c220f42804d3ce8fbefadd5f161baa2f930
Louis <louis@Galileo.local>
2016-10-15 04:43:15 +1100

    Done button reloads scene

---
+++ Assets/Scripts/DoneButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DoneButton : MonoBehaviour {
+	
+	void Awake ()
+	{
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(ReloadScene);
+	}
+	
+	void ReloadScene ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
